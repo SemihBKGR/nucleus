aa1c0300739921fcd7db220310bcd0eaafa3a093
SemihBKGR <buyukgungorsemih@gmail.com>
2022-01-22 23:59:28 +0300

    adds lru and cache test functions

--- cache_test.go
+++ cache_test.go
@@ -2,47 +2,232 @@ package nucleus
 
 import (
 	"math"
+	"strconv"
 	"testing"
 )
 
-func TestOfCache(t *testing.T) {
+func TestNewLruCache(t *testing.T) {
+	cache, err := NewLruCache(1)
+	if cache == nil {
+		t.FailNow()
+	}
+	if err != nil {
+		t.FailNow()
+	}
+	cache, err = NewLruCache(0)
+	if cache != nil {
+		t.FailNow()
+	}
+	if err == nil {
+		t.FailNow()
+	}
+	cache, err = NewLruCache(-1)
+	if cache != nil {
+		t.FailNow()
+	}
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestLruCache_Add(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity*2; i++ {
+		cache.Add(i, strconv.Itoa(i))
+	}
+}
+
+func TestLruCache_Set(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity; i++ {
+		ok := cache.Set(i, strconv.Itoa(i))
+		if ok {
+			t.FailNow()
+		}
+	}
+	for i := 0; i < capacity; i++ {
+		cache.Add(i, strconv.Itoa(i))
+	}
+	for i := 0; i < capacity; i++ {
+		ok := cache.Set(i, strconv.Itoa(i))
+		if !ok {
+			t.FailNow()
+		}
+	}
+}
+
+func TestLruCache_Get(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity; i++ {
+		cache.Add(i, strconv.Itoa(i))
+	}
+	for i := 0; i < capacity; i++ {
+		value, ok := cache.Get(i)
+		if !ok || value.(string) != strconv.Itoa(i) {
+			t.FailNow()
+		}
+	}
+	for i := capacity; i < capacity*2; i++ {
+		value, ok := cache.Get(i)
+		if ok || value != nil {
+			t.FailNow()
+		}
+	}
+}
+
+func TestLruCache_Peek(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity; i++ {
+		cache.Add(i, strconv.Itoa(i))
+	}
+	for i := 0; i < capacity; i++ {
+		value, ok := cache.Peek(i)
+		if !ok || value.(string) != strconv.Itoa(i) {
+			t.FailNow()
+		}
+	}
+	for i := capacity; i < capacity*2; i++ {
+		value, ok := cache.Peek(i)
+		if ok || value != nil {
+			t.FailNow()
+		}
+	}
+}
+
+func TestLruCache_Remove(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity; i++ {
+		cache.Add(i, strconv.Itoa(i))
+	}
+	for i := 0; i < capacity; i++ {
+		ok := cache.Remove(i)
+		if !ok {
+			t.FailNow()
+		}
+	}
+	for i := 0; i < capacity; i++ {
+		ok := cache.Remove(i)
+		if ok {
+			t.FailNow()
+		}
+	}
+}
+
+func TestLruCache_Contains(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity; i++ {
+		cache.Add(i, strconv.Itoa(i))
+	}
+	for i := 0; i < capacity; i++ {
+		if !cache.Contains(i) {
+			t.FailNow()
+		}
+	}
+	for i := capacity; i < capacity*2; i++ {
+		if cache.Contains(i) {
+			t.FailNow()
+		}
+	}
+}
+
+func TestLruCache_Cap(t *testing.T) {
 	capacity := 10
 	cache, _ := NewLruCache(capacity)
 	if cache.Cap() != capacity {
-		t.Fatalf("cap() != %d", capacity)
+		t.FailNow()
 	}
+}
+
+func TestLruCache_Len(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
 	if cache.Len() != 0 {
-		t.Fatalf("len() != %d", cache.Len())
+		t.FailNow()
 	}
 	for i := 0; i < capacity*2; i++ {
-		cache.Add(i, i)
+		cache.Add(i, strconv.Itoa(i))
 		if cache.Len() != int(math.Min(float64(i+1), float64(capacity))) {
-			t.Fatalf("len() != %d", i)
+			t.FailNow()
 		}
 	}
+}
 
+func TestLruCache_Clear(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity; i++ {
+		cache.Add(i, strconv.Itoa(i))
+	}
+	cache.Clear()
+	if cache.Len() != 0 {
+		t.FailNow()
+	}
+}
+
+func TestLruCache_ReCap(t *testing.T) {
+	capacity := 10
+	newCapacity := 20
+	cache, _ := NewLruCache(capacity)
+	err := cache.ReCap(newCapacity)
+	if err != nil {
+		t.FailNow()
+	}
+	if cache.Cap() != newCapacity {
+		t.FailNow()
+	}
+	newCapacity = 5
+	err = cache.ReCap(newCapacity)
+	if err != nil {
+		t.FailNow()
+	}
+	if cache.Cap() != newCapacity {
+		t.FailNow()
+	}
+	newCapacity = -1
+	err = cache.ReCap(newCapacity)
+	if err == nil {
+		t.FailNow()
+	}
+	if cache.Cap() == newCapacity {
+		t.FailNow()
+	}
+}
+
+func TestLruCache_Keys(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
 	for i := 0; i < capacity*2; i++ {
-		ok := cache.Contains(i)
-		if i/capacity < 1 {
-			if ok {
-				t.Fatalf("key '%d' must not be exists", i)
-			}
-		} else {
-			if !ok {
-				t.Fatalf("key '%d' must be exists", i)
-			}
+		cache.Add(i, strconv.Itoa(i))
+		keys := cache.Keys()
+		if !contains(keys, i) {
+			t.FailNow()
 		}
-
-		for i := capacity; i < capacity*2; i++ {
-			value, ok := cache.Get(i)
-			if !ok {
-				t.Fatalf("key '%d' does not exist", i)
-			}
-			if value.(int) != i {
-				t.Fatalf("value != %d", i)
-			}
-		}
-
 	}
+}
 
+func TestLruCache_Values(t *testing.T) {
+	capacity := 10
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity*2; i++ {
+		cache.Add(i, strconv.Itoa(i))
+		values := cache.Values()
+		if !contains(values, strconv.Itoa(i)) {
+			t.FailNow()
+		}
+	}
+}
+
+func contains(s []interface{}, e interface{}) bool {
+	for _, c := range s {
+		if c == e {
+			return true
+		}
+	}
+	return false
 }
--- lru_test.go
+++ lru_test.go
@@ -2,47 +2,183 @@ package nucleus
 
 import (
 	"math"
+	"strconv"
 	"testing"
 )
 
-func TestOfLru(t *testing.T) {
+func TestNewLru(t *testing.T) {
+	lru, err := newLru(1)
+	if lru == nil {
+		t.FailNow()
+	}
+	if err != nil {
+		t.FailNow()
+	}
+	lru, err = newLru(0)
+	if lru != nil {
+		t.FailNow()
+	}
+	if err == nil {
+		t.FailNow()
+	}
+	lru, err = newLru(-1)
+	if lru != nil {
+		t.FailNow()
+	}
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestLru_add(t *testing.T) {
 	capacity := 10
-	lru, _ := newLruCache(capacity)
+	cache, _ := NewLruCache(capacity)
+	for i := 0; i < capacity*2; i++ {
+		cache.Add(i, strconv.Itoa(i))
+	}
+}
+
+func TestLru_get(t *testing.T) {
+	capacity := 10
+	lru, _ := newLru(capacity)
+	for i := 0; i < capacity; i++ {
+		lru.add(i, strconv.Itoa(i))
+	}
+	for i := 0; i < capacity; i++ {
+		value, ok := lru.get(i, true)
+		if !ok || value.(string) != strconv.Itoa(i) {
+			t.FailNow()
+		}
+	}
+	for i := capacity; i < capacity*2; i++ {
+		value, ok := lru.get(i, true)
+		if ok || value != nil {
+			t.FailNow()
+		}
+	}
+}
+
+func TestLru_remove(t *testing.T) {
+	capacity := 10
+	lru, _ := newLru(capacity)
+	for i := 0; i < capacity; i++ {
+		lru.add(i, strconv.Itoa(i))
+	}
+	for i := 0; i < capacity; i++ {
+		ok := lru.remove(i)
+		if !ok {
+			t.FailNow()
+		}
+	}
+	for i := 0; i < capacity; i++ {
+		ok := lru.remove(i)
+		if ok {
+			t.FailNow()
+		}
+	}
+}
+
+func TestLru_contains(t *testing.T) {
+	capacity := 10
+	lru, _ := newLru(capacity)
+	for i := 0; i < capacity; i++ {
+		lru.add(i, strconv.Itoa(i))
+	}
+	for i := 0; i < capacity; i++ {
+		if !lru.contains(i) {
+			t.FailNow()
+		}
+	}
+	for i := capacity; i < capacity*2; i++ {
+		if lru.contains(i) {
+			t.FailNow()
+		}
+	}
+}
+
+func TestLru_cap(t *testing.T) {
+	capacity := 10
+	lru, _ := newLru(capacity)
 	if lru.cap() != capacity {
-		t.Fatalf("cap() != %d", capacity)
+		t.FailNow()
 	}
+}
+
+func TestLru_len(t *testing.T) {
+	capacity := 10
+	lru, _ := newLru(capacity)
 	if lru.len() != 0 {
-		t.Fatalf("len() != %d", lru.len())
+		t.FailNow()
 	}
 	for i := 0; i < capacity*2; i++ {
-		lru.add(i, i)
+		lru.add(i, strconv.Itoa(i))
 		if lru.len() != int(math.Min(float64(i+1), float64(capacity))) {
-			t.Fatalf("len() != %d", i)
+			t.FailNow()
 		}
 	}
+}
 
+func TestLru_Purge(t *testing.T) {
+	capacity := 10
+	lru, _ := newLru(capacity)
+	for i := 0; i < capacity; i++ {
+		lru.add(i, strconv.Itoa(i))
+	}
+	lru.purge()
+	if lru.len() != 0 {
+		t.FailNow()
+	}
+}
+
+func TestLru_reCap(t *testing.T) {
+	capacity := 10
+	newCapacity := 20
+	lru, _ := newLru(capacity)
+	err := lru.reCap(newCapacity)
+	if err != nil {
+		t.FailNow()
+	}
+	if lru.cap() != newCapacity {
+		t.FailNow()
+	}
+	newCapacity = 5
+	err = lru.reCap(newCapacity)
+	if err != nil {
+		t.FailNow()
+	}
+	if lru.cap() != newCapacity {
+		t.FailNow()
+	}
+	newCapacity = -1
+	err = lru.reCap(newCapacity)
+	if err == nil {
+		t.FailNow()
+	}
+	if lru.cap() == newCapacity {
+		t.FailNow()
+	}
+}
+
+func TestLru_keys(t *testing.T) {
+	capacity := 10
+	lru, _ := newLru(capacity)
 	for i := 0; i < capacity*2; i++ {
-		ok := lru.contains(i)
-		if i/capacity < 1 {
-			if ok {
-				t.Fatalf("key '%d' must not be exists", i)
-			}
-		} else {
-			if !ok {
-				t.Fatalf("key '%d' must be exists", i)
-			}
+		lru.add(i, strconv.Itoa(i))
+		keys := lru.keys()
+		if !contains(keys, i) {
+			t.FailNow()
 		}
-
-		for i := capacity; i < capacity*2; i++ {
-			value, ok := lru.get(i, false)
-			if !ok {
-				t.Fatalf("key '%d' does not exist", i)
-			}
-			if value.(int) != i {
-				t.Fatalf("value != %d", i)
-			}
-		}
-
 	}
+}
 
+func TestLru_values(t *testing.T) {
+	capacity := 10
+	cache, _ := newLru(capacity)
+	for i := 0; i < capacity*2; i++ {
+		cache.add(i, strconv.Itoa(i))
+		values := cache.values()
+		if !contains(values, strconv.Itoa(i)) {
+			t.FailNow()
+		}
+	}
 }
